4af68dfc60c4b1b80bee7a4a66444aaeb1e4c5bc
joe miller <joeym@joeym.net>
2012-04-25 16:43:57 -0700

    [COOK-1227] flush internal yum cache after adding a new repo

--- providers/repository.rb
+++ providers/repository.rb
@@ -33,6 +33,13 @@ action :add do
     execute "yum -q makecache" do
       action :nothing
     end
+    #reload internal Chef yum cache
+    ruby_block "reload-internal-yum-cache" do
+      block do
+        Chef::Provider::Package::Yum::YumCache.instance.reload
+      end
+      action :nothing
+    end
     #write out the file
     template "/etc/yum.repos.d/#{new_resource.repo_name}.repo" do
       cookbook "yum"
@@ -50,6 +57,7 @@ action :add do
                   :bootstrapurl => new_resource.bootstrapurl
                 })
       notifies :run, resources(:execute => "yum -q makecache"), :immediately
+      notifies :create, resources(:ruby_block => "reload-internal-yum-cache"), :immediately
     end
   end
 end
