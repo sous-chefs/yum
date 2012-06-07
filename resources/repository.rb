3459cf3c5d94c2257f69448b6bd185c0c3dc0bbe
Eric Edgar <rocketman110@gmail.com>
2012-06-07 16:26:31 -0500

    Add includepkgs support to yum repositories

--- resources/repository.rb
+++ resources/repository.rb
@@ -29,6 +29,7 @@ attribute :enabled, :default => 1
 attribute :type, :kind_of => String, :default => nil
 attribute :failovermethod, :kind_of => String, :default => nil
 attribute :bootstrapurl, :kind_of => String, :default => nil
+attribute :includepkgs, :kind_of => String, :default => nil
 
 def initialize(*args)
   super
