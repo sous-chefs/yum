f083ec55623dd68ab5c0e856edd8948dfe29ee89
Eric G. Wolfe <wolfe21@marshall.edu>
2012-11-05 16:04:51 -0500

    Correct amazon edge cases
    Amazon platform_version differs from other RHEL families.
    
    This commit should correct the attribute data for Amazon platform.

--- attributes/epel.rb
+++ attributes/epel.rb
@@ -18,11 +18,18 @@
 # limitations under the License.
 #
 
-default['yum']['epel']['url'] = "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{node['platform_version'].to_i}&arch=$basearch"
-
-if node['platform_version'].to_i >= 6
-  set['yum']['epel']['key'] = "RPM-GPG-KEY-EPEL-6"
+case node['platform']
+when "amazon"
+  default['yum']['epel']['url'] = "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch"
+  default['yum']['epel']['key'] = "RPM-GPG-KEY-EPEL-6"
 else
-  set['yum']['epel']['key'] = "RPM-GPG-KEY-EPEL"
+  default['yum']['epel']['url'] = "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{node['platform_version'].to_i}&arch=$basearch"
+
+  if node['platform_version'].to_i >= 6
+    default['yum']['epel']['key'] = "RPM-GPG-KEY-EPEL-6"
+  else
+    default['yum']['epel']['key'] = "RPM-GPG-KEY-EPEL"
+  end
 end
+
 default['yum']['epel']['key_url'] = "http://download.fedoraproject.org/pub/epel/#{node['yum']['epel']['key']}"
