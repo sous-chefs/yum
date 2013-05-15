716ef054f48468bf20b0a22b22a150735c736bfc
Sean OMeara <someara@opscode.com>
2013-05-15 17:39:34 -0400

    adding support for internal repo

---
+++ attributes/internal.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: yum
+# Attributes:: internal
+#
+# Author:: Sean OMeara <someara@opscode.com>
+# Copyright 2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['yum']['internal']['key'] = "RPM-GPG-KEY-EPEL-6"
+default['yum']['internal']['key_url'] = "http://33.33.33.10/centos/6.3/os/x86_64/RPM-GPG-KEY-CentOS-6"
+default['yum']['internal']['url'] = "http://33.33.33.10/centos/6.3/os/x86_64/"
---
+++ recipes/internal.rb
@@ -0,0 +1,49 @@
+#
+# Author:: Sean OMeara (<someara@opscode.com>)
+# Cookbook Name:: yum
+# Recipe:: internal
+#
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+file "/etc/yum.repos.d/CentOS-Base.repo" do
+  action :delete
+end
+
+file "/etc/yum.repos.d/CentOS-Debuginfo.repo" do
+  action :delete
+end
+
+file "/etc/yum.repos.d/CentOS-Media.repo" do
+  action :delete
+end
+
+file "/etc/yum.repos.d/CentOS-Vault.repo" do
+  action :delete
+end
+
+##
+
+yum_key node['yum']['internal']['key'] do
+  url  node['yum']['internal']['key_url']
+  action :add
+end
+
+yum_repository "internal" do
+  description "Local yum mirror of Enterprise Linux"
+  url node['yum']['internal']['url']
+  key node['yum']['internal']['key']
+end
+
