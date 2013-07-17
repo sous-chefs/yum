c19385bb3c1a121968068a0d72fbf3bf53002a4c
James Cuzella <james.cuzella@lyraphase.com>
2013-07-17 16:38:22 -0600

    Warn user that yum::elrepo is not supported on Fedora
    See: http://lists.elrepo.org/pipermail/elrepo/2011-August/000799.html

--- recipes/elrepo.rb
+++ recipes/elrepo.rb
@@ -16,16 +16,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-yum_key node['yum']['elrepo']['key'] do
-  url  node['yum']['elrepo']['key_url']
-  action :add
-end
+unless platform?('fedora')
 
-yum_repository "elrepo" do
-  description "ELRepo.org Community Enterprise Linux Extras Repository"
-  key node['yum']['elrepo']['key']
-  mirrorlist node['yum']['elrepo']['url']
-  includepkgs node['yum']['elrepo']['includepkgs']
-  exclude node['yum']['elrepo']['exclude']
-  action :create
+  yum_key node['yum']['elrepo']['key'] do
+    url  node['yum']['elrepo']['key_url']
+    action :add
+  end
+
+  yum_repository "elrepo" do
+    description "ELRepo.org Community Enterprise Linux Extras Repository"
+    key node['yum']['elrepo']['key']
+    mirrorlist node['yum']['elrepo']['url']
+    includepkgs node['yum']['elrepo']['includepkgs']
+    exclude node['yum']['elrepo']['exclude']
+    action :create
+  end
+else
+  Chef::Log.warn("Fedora is not officially supported by elrepo!  (Please see: http://lists.elrepo.org/pipermail/elrepo/2011-August/000799.html)")
 end
