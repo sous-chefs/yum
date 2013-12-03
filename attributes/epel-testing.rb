bfbb5e45d3ebc538b33691b93722d577f03206ff
Arisawa Kosuke <arisawa@crazytaxi.jp>
2013-12-04 01:31:49 +0900

    Add attribute

---
+++ attributes/epel-testing.rb
@@ -0,0 +1,29 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: yum
+# Attributes:: epel-testing
+#
+# Copyright 2011, Eric G. Wolfe
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+case node['platform']
+when 'amazon'
+  default['yum']['epel-testing']['url'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=testing-epel6&arch=$basearch'
+else
+  default['yum']['epel-testing']['url'] = "http://mirrors.fedoraproject.org/mirrorlist?repo=testing-epel#{node['platform_version'].to_i}&arch=$basearch"
+end
+
+default['yum']['epel-testing']['enabled'] = '0'
