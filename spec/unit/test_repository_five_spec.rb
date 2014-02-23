8d519b3d6a36edf153dedc339271aa395e0c1d70
Jacob McCann <jmccann.git@gmail.com>
2014-02-22 18:38:52 -0600

    Update tests for yum_repository delete action
    Updating tests as part of updating yum_repository delete action to use a file
    resource instead of a template resource and adding a guard to the yum clean execute.

--- spec/unit/test_repository_five_spec.rb
+++ spec/unit/test_repository_five_spec.rb
@@ -7,8 +7,8 @@ describe 'yum_test::test_repository_four' do
       ).converge(described_recipe)
   end
 
-  let(:test_repository_four_template) do
-    test_repository_four_run.template('/etc/yum.repos.d/test4.repo')
+  let(:test_repository_four_file) do
+    test_repository_four_run.file('/etc/yum.repos.d/test4.repo')
   end
 
   context 'creating a yum_repository with :remove alias' do
@@ -17,7 +17,7 @@ describe 'yum_test::test_repository_four' do
     end
 
     it 'deletes file[/etc/yum.repos.d/test4.repo]' do
-      expect(test_repository_four_run).to delete_template('/etc/yum.repos.d/test4.repo')
+      expect(test_repository_four_run).to delete_file('/etc/yum.repos.d/test4.repo')
     end
 
     it 'does not run execute[yum clean test4]' do
@@ -29,11 +29,12 @@ describe 'yum_test::test_repository_four' do
     end
 
     it 'sends a :run to execute[yum clean test4]' do
-      expect(test_repository_four_template).to notify('execute[yum clean test4]')
+      stub_command("yum repolist | grep -P 'test4([ \t]|$)'").and_return(true)
+      expect(test_repository_four_file).to notify('execute[yum clean test4]')
     end
 
     it 'sends a :create to ruby_block[yum-cache-reload-test4]' do
-      expect(test_repository_four_template).to notify('ruby_block[yum-cache-reload-test4]')
+      expect(test_repository_four_file).to notify('ruby_block[yum-cache-reload-test4]')
     end
 
   end
--- spec/unit/test_repository_four_spec.rb
+++ spec/unit/test_repository_four_spec.rb
@@ -7,8 +7,8 @@ describe 'yum_test::test_repository_five' do
       ).converge(described_recipe)
   end
 
-  let(:test_repository_five_template) do
-    test_repository_five_run.template('/etc/yum.repos.d/test5.repo')
+  let(:test_repository_five_file) do
+    test_repository_five_run.file('/etc/yum.repos.d/test5.repo')
   end
 
   context 'deletes a yum_repository' do
@@ -17,7 +17,7 @@ describe 'yum_test::test_repository_five' do
     end
 
     it 'deletes file[/etc/yum.repos.d/test5.repo]' do
-      expect(test_repository_five_run).to delete_template('/etc/yum.repos.d/test5.repo')
+      expect(test_repository_five_run).to delete_file('/etc/yum.repos.d/test5.repo')
     end
 
     it 'does not run execute[yum clean test5]' do
@@ -29,11 +29,11 @@ describe 'yum_test::test_repository_five' do
     end
 
     it 'sends a :run to execute[yum clean test5]' do
-      expect(test_repository_five_template).to notify('execute[yum clean test5]')
+      expect(test_repository_five_file).to notify('execute[yum clean test5]')
     end
 
     it 'sends a :create to ruby_block[yum-cache-reload-test5]' do
-      expect(test_repository_five_template).to notify('ruby_block[yum-cache-reload-test5]')
+      expect(test_repository_five_file).to notify('ruby_block[yum-cache-reload-test5]')
     end
 
   end
