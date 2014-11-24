f37f33363bbb34bfb8814e136fe86031c668199a
Sean OMeara <someara@opscode.com>
2014-11-24 12:10:26 -0500

    updating CHANGELOG and bumping metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef'
 maintainer_email 'cookbooks@getchef.com'
 license 'Apache 2.0'
 description 'Configures various yum components on Red Hat-like systems'
-version '3.4.1'
+version '3.5.0'
 
 supports 'redhat'
 supports 'centos'
