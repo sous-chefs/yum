05453ce2ce4fb9ceb35311b40f8bd02171427256
Tim Smith <tsmith@chef.io>
2016-09-06 13:54:36 -0700

    Add rpm fusion repo setup on RHEL
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/yum_test/recipes/test_repo.rb
+++ test/fixtures/cookbooks/yum_test/recipes/test_repo.rb
@@ -7,4 +7,13 @@ when 'fedora'
       'metadata_expire' => '7d'
     )
   end
+else
+  yum_repository 'rpmfusion' do
+    mirrorlist 'http://mirrors.rpmfusion.org/mirrorlist?repo=free-el-updates-released-$releasever&arch=$basearch'
+    enabled true
+    options(
+      'metadata_expire' => '7d'
+    )
+    action :create
+  end
 end
