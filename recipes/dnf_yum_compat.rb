8f0670c7c2bfb2ba3e7f698c369b8c11e5d4d86b
Tim Smith <tsmith@chef.io>
2016-11-05 16:42:26 -0700

    Make cache in the DNF compat recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/dnf_yum_compat.rb
+++ recipes/dnf_yum_compat.rb
@@ -16,8 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+execute 'make yum cache' do
+  command 'yum makecache'
+  action :nothing
+end
+
 execute 'install yum' do
   command 'dnf install yum -y'
   not_if { ::File.exist?('/var/lib/yum') }
   action :nothing
+  notifies :run, 'execute[make yum cache]', :immediately
 end.run_action(:run)
