6a5e35bfd6d1f7472bc94f62dd93afafeb65ccdb
Tim Smith <tsmith@chef.io>
2017-01-17 17:27:43 -0800

    Convert to Inspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/default_specs.rb
+++ test/integration/default/default_specs.rb
@@ -1,6 +1,6 @@
 # The default recipe takes over yum_globalconfig[/etc/yum.conf]
 # Test to make sure the package manager still works.
 
-@test "install a package" {
-  yum -y install emacs-nox
-}
+describe command('yum -y install emacs-nox') do
+  its('exit_status') { should eq 0 }
+end
