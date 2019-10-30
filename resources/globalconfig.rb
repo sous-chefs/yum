ca0fda11409567b86cda9fdd045c81d7ec4dc417
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:09:23 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/globalconfig.rb
+++ resources/globalconfig.rb
@@ -3,7 +3,7 @@
 # Resource:: repository
 #
 # Author:: Sean OMeara <someara@chef.io>
-# Copyright:: 2013-2017, Chef Software, Inc.
+# Copyright:: 2013-2019, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
